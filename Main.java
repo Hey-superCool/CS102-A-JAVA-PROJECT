f4b1cdd3891c0de05e121ab157ac2143497fe565
Hey-superCool <105895326+Hey-superCool@users.noreply.github.com>
2022-05-22 15:10:37 +0800

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,12 +1,13 @@
 import view.ChessGameFrame;
+import view.SelectionFrame;
 
 import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
-            ChessGameFrame mainFrame = new ChessGameFrame(1000, 760);
-            mainFrame.setVisible(true);
+            SelectionFrame selectionFrame = new SelectionFrame(760,760);
+            selectionFrame.setVisible(true);
         });
     }
 }
