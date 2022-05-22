c317bbe5242c4f968af3817d5d6ee7ecda628261
Hey-superCool <105895326+Hey-superCool@users.noreply.github.com>
2022-05-22 15:26:05 +0800

    Update PawnChessComponent.java

--- src/model/PawnChessComponent.java
+++ src/model/PawnChessComponent.java
@@ -157,8 +157,10 @@ public class PawnChessComponent extends ChessComponent{
             g.drawImage(pawnImage, 0, 0, getWidth() , getHeight(), this);
             g.setColor(Color.BLACK);
             if (isSelected()) { // Highlights the model if selected.
-                g.setColor(Color.RED);
-                g.drawOval(0, 0, getWidth() , getHeight());
+                g.setColor(new Color(108,153,153,150));
+                g.fillRect(0, 0, getWidth() , getHeight());
+                g.drawImage(pawnImage, -7, -7, getWidth()+14 , getHeight()+14, this);
+                g.setColor(Color.BLACK);
             }
         }
 
